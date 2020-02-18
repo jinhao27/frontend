8a7b8539a5814445ec419eab9d71c24eb7ec597f
Alex Yu <alexjy@yahoo.com>
2020-02-18 16:30:31 -0500

    New react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test
+        </p>
+        <a
+          className="App-link"
+          href="https://stackd.gg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Stackd Time
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
