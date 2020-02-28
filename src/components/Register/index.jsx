0ee0874698a87381b9b70ebb10ba435f11b315cd
Alex Yu <alexjy@yahoo.com>
2020-02-28 18:45:40 -0500

    Add league image, register area

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 
 function Signup() {
   return (
     <section>
-      <h2> this is the body of the register page</h2>
+      <div class="container">
+        <div id="bg"></div>
+        <div id="register-area">
+          <h1>REGISTER</h1>
+          <button><FontAwesomeIcon id="icon" icon={faDiscord} />SIGN UP FOR DISCORD</button>
+          <a id="have-account" href="#">ALREADY HAVE AN ACCOUNT?</a>
+          <p>By signing up you agree to the Stackd <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
+        </div>
+      </div>
     </section>
   );
 }
