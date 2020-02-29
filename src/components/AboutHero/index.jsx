895081aafff83c52beb0fb8b69aeffb3363e677c
raewkl <rachelwklam@gmail.com>
2020-02-29 11:21:28 -0500

    fonts added, bg stretch fixed

--- src/components/AboutHero/index.jsx
+++ src/components/AboutHero/index.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import './styles.scss';
-import bgimage from '../../assets/images/AboutHero_bg';
-import longlogo from '../../assets/images/Logo_hwhite'
+import React from "react";
+import "./styles.scss";
+import longlogo from "../../assets/images/Logo_hwhite";
+// import bgimage from "../../assets/images/AboutHero_bg";
+// had trouble with bg stretching here so moved it to css file.
 
 function AboutHero() {
   return (
     <section>
-      <div class="bkgd">
-        <div class="hero">
-          <img src={longlogo}></img>
 
-          <div class="herotext">
+      {/* Title banner */}
+      <div class="title-bg">
+        <div class="container">
+          <img src={longlogo} id="long-logo"></img>
+          <div class="title-text">
             <h1>Our Company</h1>
           </div>
         </div>
-
-        <img src={bgimage}></img>
       </div>
-
+      
+      {/* Placeholder */}
       "Team page goes here"
 
-      <div class="banner">
-        <div class="hero">
-          <div class="bannertext">
-            <h1>Looking to chat?</h1>
-            <p>Contact us at <a href="mailto:info@stackd.gg?subject=Web Inquiry">info@stackd.gg</a></p>
-          </div>
+      {/* Email banner */}
+      <div class="banner-bg">
+        <div class="banner-text">
+          <h1>Looking to chat?</h1>
+          <p>
+            {" "}
+            Contact us at{" "}
+            <a href="mailto:info@stackd.gg?subject=Web Inquiry">
+              info@stackd.gg
+            </a>
+          </p>
         </div>
-
-        <img src={bgimage}></img>
       </div>
+
     </section>
   );
 }
