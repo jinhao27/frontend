f4d19834ad38585cd66419c2c1a19f3cdffee480
Alex Yu <alexjy@yahoo.com>
2020-02-28 19:42:47 -0500

    Complete register-area part, needs background image

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -10,9 +10,16 @@ function Signup() {
         <div id="bg"></div>
         <div id="register-area">
           <h1>REGISTER</h1>
-          <button><FontAwesomeIcon id="icon" icon={faDiscord} />SIGN UP FOR DISCORD</button>
-          <a id="have-account" href="#">ALREADY HAVE AN ACCOUNT?</a>
-          <p>By signing up you agree to the Stackd <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
+          <form>
+            <button><FontAwesomeIcon id="icon" icon={faDiscord} />SIGN UP WITH DISCORD</button>
+          </form>
+          <div id="hr-line">
+            <hr></hr>
+          </div>
+          <a id="have-account" href="/login">ALREADY HAVE AN ACCOUNT?</a>
+          <div id="terms">
+            <p>By signing up you agree to the Stackd <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
+          </div>
         </div>
       </div>
     </section>
