8e5a639dd7e49d476189eae392ef8ded76bb6f27
unknown <kasa020619@gmail.com>
2020-02-29 23:48:23 -0500

    relative link

--- src/components/EventBanner/index.jsx
+++ src/components/EventBanner/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import logo from 'C:/Users/KASA/Desktop/stackd/frontend/src/assets/images/logo.png';
+import logo from '../../assets/images/logo.png';
 
 function EventBanner() {
   return (
