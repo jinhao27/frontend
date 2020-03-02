4b99edf6932fc143f447e6de78ff6bf4ddcca608
Alex Yu <alexjy@yahoo.com>
2020-03-02 10:36:04 -0500

    Add about page body and images

--- src/routes/About/index.jsx
+++ src/routes/About/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutHero from '../../components/AboutHero';
+import AboutBody from '../../components/AboutBody';
 
 function AboutPage() {
   return (
     <section>
       <AboutHero />
+      <AboutBody />
     </section>
   );
 }
