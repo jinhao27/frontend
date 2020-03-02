2ed2a28e4fd453c94b21def27dd9a0cb4fbbd8d8
Danielle Truong <datruong@deloitte.ca>
2020-03-02 08:00:15 -0500

    made some styling fixes for header and sign up page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,25 +6,27 @@ import stackdheader from '../../assets/images/StackdHeaderText.png';
 function Header() {
   return (
     <section className='header'>
-      <div className='logo'>
-        <img src={imageheader} className='jadeheader'></img>
-        <img src={stackdheader} className='jadetext'></img>
-      </div>
-      <div className='links'>
-        <ul>
-          <li>
-            <a href='/signup'>SIGN IN</a>
-          </li>
-          <li>
-            <a href='/about'>ABOUT US</a>
-          </li>
-          <li>
-            <a href='/event'>EVENT</a>
-          </li>
-        </ul>
-        <link
-          href='https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap'
-          rel='stylesheet'></link>
+      <div className='wrapper'>
+        <div className='logo'>
+          <img src={imageheader} className='jadeheader'></img>
+          <img src={stackdheader} className='jadetext'></img>
+        </div>
+        <div className='links'>
+          <ul>
+            <li>
+              <a href='/signup'>SIGN IN</a>
+            </li>
+            <li>
+              <a href='/about'>ABOUT US</a>
+            </li>
+            <li>
+              <a href='/event'>EVENT</a>
+            </li>
+          </ul>
+          <link
+            href='https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap'
+            rel='stylesheet'></link>
+        </div>
       </div>
     </section>
   );
