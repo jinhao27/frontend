f02df61b66c584a26e1130e1cd8b2fa4db437d46
unknown <kasa020619@gmail.com>
2020-03-10 21:00:44 -0400

    done

--- src/components/DashboardHeader/index.jsx
+++ src/components/DashboardHeader/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './styles.scss';
+import samplepf from '../../assets/images/samplepf.png';
+import ratestar from '../../assets/images/ratestar.png';
+
 
 function DashboardHeader() {
   return (
-    <section>
-      <h2> this is the Dashboard page header</h2>
+    <section className="container">
+      <div className="userinfo">
+        <div className="pf">
+          <img src={samplepf} alt="profile"></img>
+        </div>
+        <div className="gamer">
+          <div className="welcome">
+            <p>Welcome,</p>
+          </div>
+      {/*gamer name, rate, and statics will be changed*/}
+          <div className ="nickname">
+            <p>gamergiiirlxo</p>
+          </div>
+          <div className="rating">
+            <img src={ratestar} alt="ratestar"></img>
+            <p>4.7</p>
+          </div>
+        </div>
+      </div>
+
     </section>
   );
 }
