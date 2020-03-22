3489b3029fb5392fe1fb33b8b914de0a83ecda23
unknown <kasa020619@gmail.com>
2020-03-22 19:41:01 -0400

    could not finish css for review page

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './styles.scss';
+import chatlogo from '../../assets/images/chatlogo.png'
+import samplechat from '../../assets/images/samplechat.png'
 
 function Chat() {
   return (
-    <section>
-      <h2> this is the chat component</h2>
+    <section className="chatcontainer">
+      <div className="top">
+        <div className="chatlogo">
+        <img src={chatlogo} alt="chatlogo"></img>
+        </div>
+        <p>Stackd Chat</p> 
+      </div>
+      <div className="samplechat">
+      <img src={samplechat} alt="samplechat"></img>
+      </div>
+      <div className="review">
+        <div className="players">
+        <p className="reviews">Review Recent Matches</p>
+          <div className="player">
+            <p>gamergirl2001</p>
+          </div>
+          <div className="player">
+            <p>league23_master</p>
+          </div>
+          <div className="player">
+            <p>gaming4ever_99</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/DashboardHeader/index.jsx
+++ src/components/DashboardHeader/index.jsx
@@ -1,31 +1,10 @@
 import React from 'react';
 import './styles.scss';
-import samplepf from '../../assets/images/samplepf.png';
-import ratestar from '../../assets/images/ratestar.png';
-
 
 function DashboardHeader() {
   return (
-    <section className="container">
-      <div className="userinfo">
-        <div className="pf">
-          <img src={samplepf} alt="profile"></img>
-        </div>
-        <div className="gamer">
-          <div className="welcome">
-            <p>Welcome,</p>
-          </div>
-      {/*gamer name, rate, and statics will be changed*/}
-          <div className ="nickname">
-            <p>gamergiiirlxo</p>
-          </div>
-          <div className="rating">
-            <img src={ratestar} alt="ratestar"></img>
-            <p>4.7</p>
-          </div>
-        </div>
-      </div>
-
+    <section>
+      <h2> this is the DashboardHeader</h2>
     </section>
   );
 }
