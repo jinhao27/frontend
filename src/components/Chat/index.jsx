1b73ab479ea7a73a7b95c6ad53b23c99773d7bdf
Danielle Truong <datruong@deloitte.ca>
2020-03-29 11:38:33 -0400

    fixed css

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import './styles.scss';
-import chatlogo from '../../assets/images/chatlogo.png'
-import samplechat from '../../assets/images/samplechat.png'
+import chatlogo from '../../assets/images/chatlogo.png';
+import samplechat from '../../assets/images/samplechat.png';
 
 function Chat() {
   return (
-    <section className="chatcontainer">
-      <div className="top">
-        <div className="chatlogo">
-        <img src={chatlogo} alt="chatlogo"></img>
+    <section className='chatcontainer'>
+      <div className='top'>
+        <div className='chatlogo'>
+          <img src={chatlogo} alt='chatlogo'></img>
         </div>
-        <p>Stackd Chat</p> 
+        <p>Stackd Chat</p>
       </div>
-      <div className="samplechat">
-      <img src={samplechat} alt="samplechat"></img>
+      <div className='samplechat'>
+        <img src={samplechat} alt='samplechat' className='samplechat'></img>
       </div>
-      <div className="review">
-        <div className="players">
-        <p className="reviews">Review Recent Matches</p>
-          <div className="player">
-            <p>gamergirl2001</p>
+      <div className='review'>
+        <div className='players'>
+          <p className='review-title'>Review Recent Matches</p>
+          <div className='player-review'>
+            <p className='player'>gamergirl2001</p>
           </div>
-          <div className="player">
-            <p>league23_master</p>
+          <div className='player-review'>
+            <p className='player'>league23_master</p>
           </div>
-          <div className="player">
-            <p>gaming4ever_99</p>
+          <div className='player-review'>
+            <p className='player'>gaming4ever_99</p>
           </div>
         </div>
       </div>
